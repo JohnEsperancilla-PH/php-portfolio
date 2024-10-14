b90c7d163474c5fae87b5072d0a8c738833243eb
JohnEsperancilla-PH <109806312+JohnEsperancilla-PH@users.noreply.github.com>
2024-10-14 14:10:20 +0800

    Revert to the Implementation of Portfolio

--- logout.php
+++ logout.php
@@ -1,16 +1,15 @@
 <?php include 'php-sections/loader.php';?>
 
 <?php 
-    session_start(); // Start the session
+session_start(); // Start the session
 
-    // Clear specific session variables if necessary
-    unset($_SESSION['username']); // Clear the username variable
+// Clear specific session variables if necessary
+unset($_SESSION['username']); // Clear the username variable
 
-    // Destroy all session data
-    session_destroy(); // Destroy the session
+// Destroy all session data
+session_destroy(); // Destroy the session
 
-    // Redirect to login page
-    header('Location: login.php'); 
-    exit();
-
+// Redirect to login page
+header('Location: login.php'); 
+exit();
 ?>
